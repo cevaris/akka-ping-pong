eec581235221fb0b181ea2f11dfefa77e865b6f4
Adam Cardenas <cevaris@gmail.com>
2015-05-05 09:08:37 -0600

    added timeout

--- src/main/scala/com/cevaris/pingpong/PingPong.scala
+++ src/main/scala/com/cevaris/pingpong/PingPong.scala
@@ -1,73 +1,69 @@
 package com.cevaris.pingpong
 
 import akka.actor._
+import akka.routing.{ Broadcast, RoundRobinPool }
 import com.typesafe.scalalogging.LazyLogging
 
-// val PROJECT_NAME    = "akkka-ping-pong"
-// val PROJECT_VERSION = "0.1"
-// val parser = new scopt.OptionParser[Config](PROJECT_NAME) {
-//   head(PROJECT_NAME, PROJECT_VERSION)
-//   opt[Int]('s', "steps") action { (x, c) =>
-//     c.copy(steps = x) } text("Number of Ping/Pong steps")
-// }
-
-trait StepMessage {
-  def next: StepMessage
-}
-case class LastStep() extends StepMessage {
-  def next: StepMessage = LastStep()
-}
-case class Step(value: Int) extends StepMessage {
-  def next: StepMessage = {
-    if (value > 0) {
-      Step(value-1)
-    } else {
-      LastStep()
-    }
+trait StepMessage
+object StepMessage {
+  def next(step: StepMessage) = step match {
+    case FinalStep()   => step
+    case Step(v: Int) if v >  0 => Step(v-1)
+    case Step(v: Int) if v <= 0 => FinalStep()
   }
 }
+case class FinalStep() extends StepMessage
+case class Step(value: Int) extends StepMessage
+
 
 case class PingMessage(step: StepMessage)
 case class PongMessage(step: StepMessage)
 
-case class StartGame(value: Int)
+case class  StartupMessage(value: Int)
+case object ShutdownMessage
 
 class Ping(pong: ActorRef) extends Actor with LazyLogging {
   def receive = {
-    case StartGame(value: Int)  =>
+    case ShutdownMessage =>
+      context.system.shutdown
+    case StartupMessage(value: Int) =>
       logger.debug("starting")
       pong ! PingMessage(Step(value))
-    case PongMessage(step: LastStep) =>
+    case PongMessage(step: FinalStep) =>
       logger.debug("stopping")
-      context.stop(self)
     case PongMessage(step: Step)  =>
       logger.debug(s"Pong $step")
-      pong ! PingMessage(step.next)
+      pong ! PingMessage(StepMessage.next(step))
   }
 }
 
 class Pong() extends Actor with LazyLogging {
   def receive = {
-    case PingMessage(step: LastStep) =>
+    case ShutdownMessage => context.system.shutdown
+    case PingMessage(step: FinalStep) =>
       logger.debug("stopping")
-      context.stop(self)
     case PingMessage(step: Step)  =>
       logger.debug(s"Ping $step")
-      sender ! PongMessage(step.next)
+      sender ! PongMessage(StepMessage.next(step))
   }
 }
 
 object PingPongApp extends App with LazyLogging {
 
+  val MaxThreads = 5
+  val router = RoundRobinPool(MaxThreads)
   val system = ActorSystem("PingPongApp")
-  val pong = system.actorOf(Props[Pong], name= "pong")
-  val ping = system.actorOf(Props(new Ping(pong)), name= "ping")
+  val pong = system.actorOf(Props[Pong].withRouter(router), name="pong")
+  val ping = system.actorOf(Props(new Ping(pong)), name="ping")
 
   args.toList match {
     case steps :: Nil =>
-      ping ! StartGame(steps.toInt)
+      for (n <- 0 until MaxThreads-1)
+        ping ! StartupMessage(steps.toInt)
     case _ =>
-      ping ! StartGame(15)
+      ping ! StartupMessage(15)
   }
 
+  Thread.sleep(2000)
+  system.shutdown
 }
